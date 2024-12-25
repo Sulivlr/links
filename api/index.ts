7cf9abdeb2876fd985e152379c1e1d3ae04f86c2
Sultan Mukhtarov <suli419956@gmail.com>
2024-12-25 07:42:54 +0600

    created post and get by id req with mongoose, also created model.

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import linkRouter from './routers/links';
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use('links/', linkRouter)
 
 const run = async () => {
   await mongoose.connect('mongodb://localhost/links');
--- api/routers/links.ts
+++ api/routers/links.ts
@@ -1,19 +1,40 @@
 import express from 'express';
+import Link from '../models/Link';
+
 
 const linkRouter = express.Router();
 
-linkRouter.get('/', async (req, res, next) => {
-  try {
-
-  } catch (error) {
-    next(error);
-  }
-});
+const generateShortUrl = (length = 7) => Math.random().toString(36).substr(2, length);
 
 linkRouter.post('/', async (req, res, next) => {
   try {
-
+    if (!req.body.url) {
+      res.status(400).send({error: 'URL is required'});
+    }
+    const shortUrl = generateShortUrl();
+    const link = new Link({
+      originalUrl: req.body.url,
+      shortUrl
+    });
+    await link.save();
+    res.json(link);
   } catch (error) {
     next(error);
   }
 });
+
+linkRouter.get('/:shortUrl', async (req, res, next) => {
+  try {
+    const link = await Link.findOne({shortUrl: req.params.shortUrl});
+    if (link) {
+      res.status(301).redirect(link.originalUrl);
+    } else {
+      res.status(404).send({error: 'Not Found'});
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+export default linkRouter;
